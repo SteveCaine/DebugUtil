b68015023f5eefe79e856ec44c516892436acb60
Steve Caine <scaine@mac.com>
2015-02-08 15:32:41 -0500

    Update copyright notice to MIT license. Update file headers with name of this Xcode project.

--- Debug_JSON.h
+++ Debug_JSON.h
@@ -3,7 +3,10 @@
 //	version 0.0
 //
 //	Created by Steve Caine on 08/01/14.
-//  Copyright (c) 2014 Steve Caine. All rights reserved.
+//
+//	This code is distributed under the terms of the MIT license.
+//
+//	Copyright (c) 2014 Steve Caine.
 //
 
 //#import <Foundation/Foundation.h>
